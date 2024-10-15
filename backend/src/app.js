efd49fa09bdb85f0e6a7810553a6e296c19de331
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-15 22:23:54 +0530

    feat: Implement middleware, configure routes, and enable CORS

---
+++ backend/src/app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+
+const app = express();
+
+// middleware for communication with the client side
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+}))
+
+// middleware for accepting form data
+app.use(express.json());
+
+// middleware for acception url data
+app.use(express.urlencoded({ extended: true }));
+
+// middleware for handling images
+app.use(express.static(path.join(__dirname, 'public')));
+
+import categoryRouter from './routes/category.routes.js';
+
+app.use("/api/categories", categoryRouter);
+
+app.get("/", (req, res) => {
+    res.send("Hello from the server side!");
+})
+
+
+
+export default app;
