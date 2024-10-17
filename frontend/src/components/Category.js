5c06cd6d7a7985b348e904666c26dc6ac73159f6
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-17 17:31:42 +0530

    refactore: handle CRUD operation using useContext hook

--- frontend/src/components/Category.js
+++ frontend/src/components/Category.js
@@ -1,34 +1,50 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React, { useContext, useEffect, useState } from 'react';
+import { CategoryContext } from '../contexts/CategoryContext';
 
 const Category = () => {
+  const { addCategories, updateCategory, currentCategory, setCurrentCategory } = useContext(CategoryContext);
 
-  const [name, setName] = useState('');
-  const [image, setImage] = useState(null);
-  const [status, setStatus] = useState("Inactive");
+  const [categoryItems, setCategoryItems] = useState({ name: '', image: '', status: true });
+
+  useEffect(() => {
+    if (currentCategory) {
+      setCategoryItems(currentCategory);
+    } else {
+      setCategoryItems({ name: '', image: '', status: true });
+    }
+  }, [currentCategory]);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
     const formData = new FormData();
-    formData.append("name", name);
-    formData.append("image", image);
-    formData.append("status", status);
+    formData.append("name", categoryItems.name);
+    formData.append("image", categoryItems.image);
+    formData.append("status", categoryItems.status ? 'Active' : 'Inactive');
 
-    try {
-      const response = await axios.post("http://localhost:5000/api/categories/addCategory", formData, {
-        headers: {
-          "Content-Type": "multipart/form-data"
-        }
-      });
-      console.log("Category created successfully: ", response.data);
-    } catch (error) {
-      console.log("Error creating category: ", error);
+    if (categoryItems.id) {
+      await updateCategory(currentCategory.id, formData);
+    } else {
+      await addCategories(formData);
     }
 
-    setName('');
-    setImage(null);
-    setStatus("Inactive");
+    setCategoryItems({ name: '', image: '', status: true });
+    setCurrentCategory([]);
+  }
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setCategoryItems((prevCategoryItems) => ({
+      ...prevCategoryItems,
+      [name]: name === 'status' ? value === 'Active' : value,
+    }));
+  }
+
+  const handleImage = (e) => {
+    setCategoryItems((prevCategoryItems) => ({
+      ...prevCategoryItems,
+      image: e.target.files[0],
+    }));
   }
 
   return (
@@ -38,20 +54,20 @@ const Category = () => {
         <form className='flex flex-col w-full my-3 gap-2' onSubmit={handleSubmit}>
           <div>
             <label htmlFor="cname" className='block'>Name</label>
-            <input type="text" name="name" id="name" value={name} onChange={(e) => setName(() => e.target.value)} className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400' />
+            <input type="text" name="name" id="name" value={categoryItems.name} onChange={handleOnChange} className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400' autoFocus />
           </div>
           <div>
             <label htmlFor="cimg" className='block'>Upload Image</label>
-            <input type="file" name="image" id="image" onChange={(e) => setImage(() => e.target.files[0])} className='bg-gray-100 w-full rounded-md p-2 border border-gray-300 shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 hover:cursor-pointer' accept='image/*' />
+            <input type="file" name="image" id="image" onChange={handleImage} className='bg-gray-100 w-full rounded-md p-2 border border-gray-300 focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 hover:cursor-pointer' accept='image/*' />
           </div>
           <div>
             <label htmlFor="status" className='block'>Status</label>
-            <select name="status" value={status} id="status" onChange={(e) => setStatus(e.target.value)} className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400'>
+            <select name="status" value={categoryItems.status ? "Active" : "Inactive"} id="status" onChange={handleOnChange} className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400'>
               <option value="Active">Active</option>
               <option value="Inactive">Inactive</option>
             </select>
           </div>
-          <button type="submit" className='bg-blue-600 text-white self-start py-1 px-4 mt-3 text-lg rounded-lg hover:bg-blue-700'>Add</button>
+          <button type="submit" className='bg-blue-600 text-white self-start py-1 px-4 mt-3 text-lg rounded-lg hover:bg-blue-700'>{categoryItems.id ? 'Update' : 'Add'}</button>
         </form>
       </div>
     </div>
--- frontend/src/components/CategoryItems.js
+++ frontend/src/components/CategoryItems.js
@@ -1,74 +1,9 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { Modal, Button } from 'react-bootstrap';
+import React, { useContext } from 'react'
+import { CategoryContext } from '../contexts/CategoryContext';
 
 const CategoryItems = () => {
 
-  const [categorys, setCategorys] = useState([])
-  const [currentCategory, setCurrentCategory] = useState({ name: '', image: '', status: 'Inactive' });
-  const [editMode, setEditMode] = useState(false);
-
-  const getDetails = async () => {
-    await axios.get("http://localhost:5000/api/categories/getCategory")
-      .then(response => setCategorys(response.data));
-  }
-
-  useEffect(() => {
-    getDetails();
-  }, []);
-
-  const handleDelete = async (id) => {
-    try {
-      await axios.delete(`http://localhost:5000/api/categories/deleteCategory/${id}`);
-      getDetails();
-    } catch (error) {
-      console.log("Error while deleting category: ", error);
-    }
-  }
-
-  const handleUpdate = (category) => {
-    setCurrentCategory(category);
-    setEditMode(true);
-  };
-
-  const handleOnChange = (e) => {
-    const { name, value } = e.target;
-    setCurrentCategory((prevCategory) => ({
-      ...prevCategory,
-      [name]: value,
-    }));
-  };
-
-  const handleImageChange = (e) => {
-    setCurrentCategory((prevCategory) => ({
-      ...prevCategory,
-      image: e.target.files[0],
-    }));
-  };
-
-  const handleOnSubmit = async (e) => {
-    e.preventDefault();
-    const formData = new FormData();
-    formData.append('name', currentCategory.name);
-    formData.append('status', currentCategory.status);
-    if (currentCategory.image) {
-      formData.append('image', currentCategory.image);
-    }
-
-    try {
-      await axios.put(`http://localhost:5000/api/categories/updateCategory/${currentCategory.id}`, formData,
-        {
-          headers: {
-            'Content-Type': "multipart/form-data",
-          }
-        }
-      );
-      getDetails();
-      setEditMode(false);
-    } catch (error) {
-      console.error('Error updating category:', error);
-    }
-  };
+  const { categorys, deleteCategory, setCurrentCategory } = useContext(CategoryContext);
 
   return (
     <div>
@@ -92,74 +27,14 @@ const CategoryItems = () => {
                 </td>
                 <td className='border border-slate-700 px-2'>{item.status}</td>
                 <td className='border border-slate-700 px-2'>
-                  <button className='m-1 bg-green-500 hover:bg-green-600 text-white rounded-md sm:px-4 px-3 py-1' onClick={() => handleUpdate(item)}>Edit</button>
-                  <button className='m-1 bg-red-500 hover:bg-red-600 text-white rounded-md sm:px-4 px-2 py-1' onClick={() => handleDelete(item.id)}>Delete</button>
+                  <button className='m-1 bg-green-500 hover:bg-green-600 text-white rounded-md sm:px-4 px-3 py-1' onClick={() => setCurrentCategory(item)}>Edit</button>
+                  <button className='m-1 bg-red-500 hover:bg-red-600 text-white rounded-md sm:px-4 px-2 py-1' onClick={() => deleteCategory(item.id)}>Delete</button>
                 </td>
               </tr>
             )}
           </tbody>
 
         </table>
-
-        {/* Modal for edit category */}
-        <Modal show={editMode} onHide={() => setEditMode(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>Edit Category</Modal.Title>
-          </Modal.Header>
-          <Modal.Body>
-            <form className='flex flex-col w-full my-3 gap-2'>
-              <div>
-                <label htmlFor='cname' className='block'>
-                  Name
-                </label>
-                <input
-                  type='text'
-                  name='name'
-                  id='name'
-                  value={currentCategory.name}
-                  onChange={handleOnChange}
-                  className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400'
-                />
-              </div>
-              <div>
-                <label htmlFor='cimg' className='block'>
-                  Upload Image
-                </label>
-                <input
-                  type='file'
-                  name='image'
-                  id='image'
-                  onChange={handleImageChange}
-                  className='bg-gray-100 w-full rounded-md p-2 border border-gray-300 shadow-sm focus:outline-none focus:ring-1 focus:ring-gray-400 focus:border-gray-400 hover:cursor-pointer'
-                  accept='image/*'
-                />
-              </div>
-              <div>
-                <label htmlFor='status' className='block'>
-                  Status
-                </label>
-                <select
-                  name='status'
-                  id='status'
-                  value={currentCategory.status}
-                  onChange={handleOnChange}
-                  className='border border-gray-300 py-1 px-2 focus:outline-none focus:border-gray-400 w-full rounded-md focus:ring-1 focus:ring-gray-400'
-                >
-                  <option value='Active'>Active</option>
-                  <option value='Inactive'>Inactive</option>
-                </select>
-              </div>
-            </form>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button variant='secondary' onClick={() => setEditMode(false)}>
-              Close
-            </Button>
-            <Button variant='primary' onClick={handleOnSubmit}>
-              Save changes
-            </Button>
-          </Modal.Footer>
-        </Modal>
       </div>
     </div>
   )
