1053e863e2d2af203a8a1f33f0c2fa72d881415e
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-18 16:15:04 +0530

    feat: add route for product CRUD operation

--- backend/src/routes/category.routes.js
+++ backend/src/routes/category.routes.js
@@ -1,6 +1,7 @@
 import { Router } from "express";
 import { addCategory, deleteCategory, getCategory, updateCategory } from "../controllers/category.controller.js";
 import { upoload } from "../middlewares/multer.middleware.js";
+import { addProduct, deleteProduct, getProducts, updateProduct } from "../controllers/product.controller.js";
 
 const router = Router();
 
@@ -15,5 +16,17 @@ router.route('/deleteCategory/:id').delete(deleteCategory);
 
 // edit category
 router.route('/updateCategory/:id').put(upoload.single('image'), updateCategory);
+
+// add product
+router.route("/addProduct").post(upoload.single('image'), addProduct);
+
+// get products
+router.route("/getProducts").get(getProducts);
+
+// delete Product
+router.route("/deleteProduct/:id").delete(deleteProduct);
+
+// update product
+router.route("/updateProduct/:id").put(upoload.single('image'), updateProduct);
 
 export default router;
