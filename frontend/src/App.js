323d525c9752c89b16fa890d455c7c7ad1b1da7c
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-18 16:10:35 +0530

    refactore: update the file for product data and react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,9 @@
-import React, { useState } from 'react'
-import Category from './components/Category'
-import CategoryItems from './components/CategoryItems'
-import { CategoryProvider } from './contexts/CategoryContext.js';
+import React from 'react'
 
 const App = () => {
 
-  const [currentCategory, setCurrentCategory] = useState([]);
-
   return (
     <>
-      <CategoryProvider>
-        <Category currentCategory={currentCategory} setCurrentCategory={setCurrentCategory} />
-        <CategoryItems setCurrentCategory={setCurrentCategory} />
-      </CategoryProvider>
     </>
   )
 }
