70b8733b3676f0146a8e5e25866f9eca68924761
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-22 18:20:35 +0530

    fix #6 : add filters and improve the image and status table field

--- frontend/src/pages/Category/CategoryItems.jsx
+++ frontend/src/pages/Category/CategoryItems.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faSortDown, faSortUp, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faSort, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { CategoryContext } from '../../contexts';
 
 
@@ -10,25 +10,43 @@ const CategoryItems = ({ setCurrentCategory }) => {
 
     const { categorys, deleteCategory } = useContext(CategoryContext);
 
+    const [sortConfig, setSortConfig] = useState({ field: 'name', isAscending: true });
+
+    const sortingAlgo = async (field) => {
+
+        const isAscending = sortConfig.field === field ? !sortConfig.isAscending : true;
+
+        categorys.sort((a, b) => {
+            if (isAscending) {
+                return a[field].localeCompare(b[field]);
+            } else {
+                return b[field].localeCompare(a[field]);
+            }
+        });
+        setSortConfig({ field, isAscending });
+    }
+
     return (
         <div>
             <div className='md:mx-5 md:px-4 sm:mx-4 sm:px-3 mx-3 px-2'>
                 <h1 className='font-bold sm:text-3xl text-2xl'>Available Category</h1>
-                <div className='overflow-x-scroll'>
-                    <div className='my-2 flex flex-row-reverse gap-1'>
-                        <button className='bg-gray-200 px-2 pt-1 rounded'>
-                            <FontAwesomeIcon icon={faSortUp} />
-                        </button>
-                        <button className='bg-gray-200 px-2 pb-1 rounded'>
-                            <FontAwesomeIcon icon={faSortDown} />
-                        </button>
-                    </div>
+                <div className='overflow-x-scroll my-2 gap-1'>
                     <table className='table table-auto border-collapse border border-slate-500 w-full'>
                         <thead>
                             <tr>
-                                <th className='border border-slate-600'>Name</th>
+                                <th className='border border-slate-600'>Name
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('name')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
+                                <th className='border border-slate-600'>Status
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('status')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
                                 <th className='border border-slate-600'>Image</th>
-                                <th className='border border-slate-600'>Status</th>
                                 <th className='border border-slate-600 w-40 sm:w-40 md:w-60 lg:w-70 xl:w-80 '>Action</th>
                             </tr>
                         </thead>
@@ -38,6 +56,11 @@ const CategoryItems = ({ setCurrentCategory }) => {
                                 {categorys.slice(0).reverse().map((item) =>
                                     <tr key={item.id}>
                                         <td className='border border-slate-700 sm:px-2 px-1'>{item.name}</td>
+                                        <td className='border border-slate-700 px-2'>
+                                            <span className={`py-1 px-3 rounded-md ${item.status === "Active" ? "text-green-500 font-medium bg-green-100" : "text-red-500 font-medium bg-red-100"}`}>
+                                                {item.status}
+                                            </span>
+                                        </td>
                                         <td className='border border-slate-700 py-1 sm:px-2 px-1'>
                                             {item.image ? (
                                                 <img className='w-14 h-14 rounded-sm' src={`http://localhost:5000/${item.image}`} alt="Category" />
@@ -48,7 +71,6 @@ const CategoryItems = ({ setCurrentCategory }) => {
                                                 )
                                             }
                                         </td>
-                                        <td className={`border border-slate-700 px-2 ${item.status === "Active" ? "text-green-500 font-medium" : "text-red-500 font-medium"}`}>{item.status}</td>
                                         <td className='border border-slate-700 px-2'>
                                             <button className='m-1 bg-green-500 hover:bg-green-600 text-white rounded-md sm:px-4 px-2 py-1' onClick={() => setCurrentCategory(item)}><FontAwesomeIcon icon={faPenToSquare} /></button>
                                             <button className='m-1 bg-red-500 hover:bg-red-600 text-white rounded-md sm:px-4 px-2 py-1' onClick={() => deleteCategory(item.id)}><FontAwesomeIcon icon={faTrash} /></button>
--- frontend/src/pages/Product/ProductItems.jsx
+++ frontend/src/pages/Product/ProductItems.jsx
@@ -1,8 +1,8 @@
 /* eslint-disable react/prop-types */
 
-import { useContext } from 'react'
+import { useContext, useState } from 'react'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPenToSquare, faSortDown, faSortUp, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { faPenToSquare, faSort, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { ProductContext } from '../../contexts';
 
 
@@ -10,27 +10,64 @@ const ProductItems = ({ setCurrentProduct }) => {
 
     const { products, deleteProduct } = useContext(ProductContext);
 
+    const [sortConfig, setSortConfig] = useState({ field: 'name', isAscending: true });
+
+    const sortingAlgo = (field) => {
+        const isAscending = sortConfig.field === field ? !sortConfig.isAscending : true;
+
+        products.sort((a, b) => {
+
+            const aValue = a[field];
+            const bValue = b[field];
+
+            if (typeof aValue === 'number' && typeof bValue === 'number') {
+                return isAscending ? aValue - bValue : bValue - aValue;
+            } else {
+                return isAscending ? aValue.localeCompare(bValue) : bValue.localeCompare(aValue);
+            }
+        })
+
+        setSortConfig({ field, isAscending });
+    }
+
     return (
         <div>
             <div className='md:mx-5 md:px-4 sm:mx-4 sm:px-3 mx-3 px-2'>
                 <h1 className='font-bold sm:text-3xl text-2xl'>Available Product</h1>
-                <div className='overflow-x-scroll'>
-                    <div className='my-2 flex flex-row-reverse gap-1'>
-                        <button className='bg-gray-200 px-2 pt-1 rounded'>
-                            <FontAwesomeIcon icon={faSortUp} />
-                        </button>
-                        <button className='bg-gray-200 px-2 pb-1 rounded'>
-                            <FontAwesomeIcon icon={faSortDown} />
-                        </button>
-                    </div>
-                    <table className='table table-auto border-collapse border border-slate-500 w-full'>
+                <div className='overflow-x-scroll my-2 gap-1'>
+                    <table className='table-auto w-full'>
                         <thead>
                             <tr>
-                                <th className='border border-slate-600'>Name</th>
-                                <th className='border border-slate-600'>Category</th>
-                                <th className='border border-slate-600'>Price</th>
-                                <th className='border border-slate-600'>Status</th>
-                                <th className='border border-slate-600'>Weight</th>
+                                <th className='border border-slate-600'>Name
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('name')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
+                                <th className='border border-slate-600'>Category
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('category')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
+                                <th className='border border-slate-600'>Price
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('price')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
+                                <th className='border border-slate-600'>Weight
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('weight')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
+                                <th className='border border-slate-600'>Status
+                                    <FontAwesomeIcon
+                                        icon={faSort}
+                                        onClick={() => sortingAlgo('status')}
+                                        className='float-end m-1 hover:cursor-pointer bg-gray-200 hover:bg-gray-300 py-1 px-2 rounded-md' />
+                                </th>
                                 <th className='border border-slate-600'>Image</th>
                                 <th className='border border-slate-600'>Action</th>
                             </tr>
@@ -43,8 +80,12 @@ const ProductItems = ({ setCurrentProduct }) => {
                                         <td className='border border-slate-700 px-2'>{item.name}</td>
                                         <td className='border border-slate-700 px-2'>{item.category}</td>
                                         <td className='border border-slate-700 px-2'>{item.price}</td>
-                                        <td className={`border border-slate-700 px-2 ${item.status === "Active" ? "text-green-500 font-medium" : "text-red-500 font-medium"}`}>{item.status}</td>
                                         <td className='border border-slate-700 px-2'>{item.weight}</td>
+                                        <td className='border border-slate-700 px-2'>
+                                            <span className={`py-1 px-3 rounded-md ${item.status === "Active" ? "text-green-500 font-medium bg-green-100" : "text-red-500 font-medium bg-red-100"}`}>
+                                                {item.status}
+                                            </span>
+                                        </td>
                                         <td className='border border-slate-700 px-2'>
                                             {item.image ? (
                                                 <img className='w-14 h-14 rounded-sm' src={`http://localhost:5000/${item.image}`} alt="product" />
