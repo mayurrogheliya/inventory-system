43ec07e2e657f56145f88817eaf99b053aacc4c4
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-27 17:27:28 +0530

    optimize dependecies by including country-state-city package

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['country-state-city'], // Ensure the dependency is included for optimization
+  },
   server: {
     proxy: {
       "/api": {
