e3ea6e05814407bf659847458b32863fcb5a2ad1
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-30 16:07:48 +0530

    Refactor CustomerMain component to use Outlet for context passing

--- frontend/src/pages/Customer/CustomerMain.jsx
+++ frontend/src/pages/Customer/CustomerMain.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react';
 import { CustomerProvider } from '../../contexts'
-import Customer from './Customer';
-import CustomerItems from './CustomerItems';
+import { Outlet } from 'react-router-dom';
 
 const CustomerMain = () => {
 
@@ -10,8 +9,7 @@ const CustomerMain = () => {
     return (
         <div className='lg:m-3 lg:p-2 md:m-4 md:p-3 m-3 p-2'>
             <CustomerProvider>
-                <Customer currentCustomer={currentCustomer} setCurrentCustomer={setCurrentCustomer} />
-                <CustomerItems setCurrentCustomer={setCurrentCustomer} />
+                <Outlet context={{ currentCustomer, setCurrentCustomer }} />
             </CustomerProvider>
         </div>
     )
