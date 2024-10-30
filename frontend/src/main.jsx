bbda9cafe2be26dad19f3e9853755d2241534c45
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-10-30 16:00:18 +0530

    style: change the position of the toast container to top-center

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -10,7 +10,7 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
     <ToastContainer
-      position='top-right'
+      position='top-center'
       autoClose={2000}
       limit={1}
       hideProgressBar={false}
