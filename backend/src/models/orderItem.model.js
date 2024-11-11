46633349066ed0c566cc2f0c15f2c9f8c3bb5fd5
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-11 22:22:11 +0530

    feat: create OrderItem model with associate to Order and ProductDetails

---
+++ backend/src/models/orderItem.model.js
@@ -0,0 +1,41 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../db/dbConfig.js";
+import Order from "./order.model.js";
+import ProductDetail from "./product.model.js";
+
+const OrderItem = sequelize.define(
+    'OrderItem',
+    {
+        orderId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'Orders',
+                key: 'id'
+            },
+            allowNull: false
+        },
+        productId: {
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'ProductDetails',
+                key: 'id'
+            },
+            allowNull: false
+        },
+        quantity: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+        },
+        price: {
+            type: DataTypes.DECIMAL(10, 2),
+            allowNull: false
+        }
+    }
+);
+
+OrderItem.associate = () => {
+    OrderItem.belongsTo(Order, { foreignKey: 'orderId', as: 'Orders' });
+    OrderItem.belongsTo(ProductDetail, { foreignKey: 'productId', as: 'ProductDetails' });
+};
+
+export default OrderItem;
