8cba2a50061bb432a70d187f1213c40d50022aea
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-11 22:29:33 +0530

    feat: establish associations between Order and OrderItem models

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,12 +1,17 @@
 import dotenv from 'dotenv';
 import app from './app.js';
 import sequelize from './db/dbConfig.js';
+import Order from './models/order.model.js';
+import OrderItem from './models/orderItem.model.js';
 
 dotenv.config({
     path: "./.env"
 })
 
 const port = process.env.PORT || 5000;
+
+Order.associate();
+OrderItem.associate();
 
 sequelize.sync()
     .then(() => {
