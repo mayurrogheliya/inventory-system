101423e788ff1af2c00714f6cc7556cde835a974
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-11 22:27:18 +0530

    feat: integrate order routes into main routes

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -2,11 +2,13 @@ import { Router } from "express";
 import categoryRoutes from "./category.routes.js";
 import productRoutes from "./product.routes.js";
 import customerRoutes from "./customer.routes.js";
+import orderRoutes from "./order.routes.js";
 
 const router = Router();
 
 router.use('/categories', categoryRoutes);
 router.use('/product', productRoutes);
 router.use('/customer', customerRoutes);
+router.use('/order', orderRoutes);
 
 export default router;
