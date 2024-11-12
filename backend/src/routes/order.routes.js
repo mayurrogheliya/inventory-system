7d00bc13d3c395a99452f6a65f6d265d4f9887a4
Mayur Rogheliya <mayurrogheliya@gmail.com>
2024-11-12 18:03:13 +0530

    feat(api): add update order route

--- backend/src/routes/order.routes.js
+++ backend/src/routes/order.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createOrder, deleteOrder, getOrder } from "../controllers/order.controller.js";
+import { createOrder, deleteOrder, getOrder, updateOrder } from "../controllers/order.controller.js";
 
 const orderRoutes = Router();
 
@@ -11,5 +11,8 @@ orderRoutes.route('/getOrder/:orderId?').get(getOrder);
 
 // delete order
 orderRoutes.route('/deleteOrder/:orderId').delete(deleteOrder);
+
+// update order
+orderRoutes.route('/updateOrder/:orderId').put(updateOrder);
 
 export default orderRoutes;
